89611d648b4d29160f2796ef2c8519fc4708bb82
Karen Collins <karentc@vt.edu>
2017-08-02 22:36:00 -0400

    Add files via upload

--- colorgame.js
+++ colorgame.js
@@ -6,48 +6,47 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
 
 
-easyBtn.addEventListener("click", function(){
-	hardBtn.classList.remove("selected");
-	easyBtn.classList.add("selected");
-	numSquares = 3;
+
+for(var i = 0; i < modeButtons.length; i++){
+	modeButtons[i].addEventListener("click", function(){
+		modeButtons[0].classList.remove("selected");
+		modeButtons[1].classList.remove("selected");
+		this.classList.add("selected");
+
+		if(this.textContent === "Easy"){
+			numSquares = 3;
+		} else {
+			numSquares = 6;
+		}
+		reset();
+	});
+}
+
+function reset(){
 	colors = generateRandomColors(numSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
+	this.textContent = "New Colors"
+	messageDisplay.textContent = "";
 	for(var i = 0; i < squares.length; i++){
 		if(colors[i]){
-			squares[i].style.backgroundColor = colors[i];
+		squares[i].style.display = "block";
+		squares[i].style.backgroundColor = colors[i];
 		} else {
 			squares[i].style.display = "none";
 		}
+		
 	}
-});
+	h1.style.backgroundColor = "#steelblue";
 
-hardBtn.addEventListener("click", function(){
-	easyBtn.classList.remove("selected");
-	hardBtn.classList.add("selected");
-	numSquares = 6;
-	colors = generateRandomColors(numSquares);
-	pickedColor = pickColor();
-	colorDisplay.textContent = pickedColor;
-	for(var i = 0; i < squares.length; i++){
-			squares[i].style.backgroundColor = colors[i];
-			squares[i].style.display = "block";
-	}
-});
+}
 
 
 resetButton.addEventListener("click", function(){
-	colors = generateRandomColors(numSquares);
-	pickedColor = pickColor();
-	colorDisplay.textContent = pickedColor;
-	for(var i = 0; i < squares.length; i++){
-		squares[i].style.backgroundColor = colors[i];
-	}
-	h1.style.backgroundColor = "#steelblue";
+	reset();
 })
 
 colorDisplay.textContent = pickedColor;
